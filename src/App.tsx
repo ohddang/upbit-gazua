6c71b191d10eb650662b7e93046c5c0473f41e03
ohddang <ohddang509@gmail.com>
2024-02-25 17:00:28 +0900

    feat: popup ui 수정 및 chrome storage 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,99 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const getStorage = async () => {
-  const storage = await chrome.storage.local.get(['distribution']);
+const getStorage = async (key: string) => {
+  const storage = await chrome.storage.local.get([key]);
   return storage;
 };
 
 const App = () => {
-  const [distribution, setDistribution] = useState<number>(1);
-  const buttonContainerRef = React.useRef<HTMLDivElement>(null);
+  const [minute, setMinute] = useState<number>(1);
+  const [showCount, setShowCount] = useState<number>(3);
 
-  const onHandleClick = (min: number) => {
-    chrome.storage.local.set({ distribution: min }, function () {
-      console.log('Value is set to ' + min);
-      setDistribution(min);
+  const minuteGroupRef = React.useRef<HTMLDivElement>(null);
+  const showCountGroupRef = React.useRef<HTMLDivElement>(null);
+
+  const onClickMinute = (min: number) => {
+    chrome.storage.local.set({ minute: min }, function () {
+      setMinute(min);
     });
   };
 
-  buttonContainerRef.current?.childNodes.forEach((button) => {
-    (button as HTMLButtonElement).id === `min_${distribution}`
-      ? (button as HTMLButtonElement).classList.add('selected')
-      : (button as HTMLButtonElement).classList.remove('selected');
-  });
+  const onClickShowCount = (count: number) => {
+    chrome.storage.local.set({ showCount: count }, function () {
+      setShowCount(count);
+    });
+  };
 
   useEffect(() => {
-    getStorage().then((storage) => {
-      setDistribution(storage.distribution || 1);
+    minuteGroupRef.current?.childNodes.forEach((button) => {
+      (button as HTMLButtonElement).id === `minute_${minute}`
+        ? (button as HTMLButtonElement).classList.add("selected")
+        : (button as HTMLButtonElement).classList.remove("selected");
+    });
+  }, [minute]);
+
+  useEffect(() => {
+    showCountGroupRef.current?.childNodes.forEach((button) => {
+      (button as HTMLButtonElement).id === `showCount_${showCount}`
+        ? (button as HTMLButtonElement).classList.add("selected")
+        : (button as HTMLButtonElement).classList.remove("selected");
+    });
+  }, [showCount]);
+
+  useEffect(() => {
+    getStorage("minute").then((storage) => {
+      setMinute(storage.minute || 1);
+    });
+    getStorage("showCount").then((storage) => {
+      setShowCount(storage.showCount || 3);
     });
   }, []);
 
   return (
     <div className="App">
-      <div className="distribution_container" ref={buttonContainerRef}>
-        <button id="min_1" onClick={() => onHandleClick(1)}>
-          1
+      <div className="button_title">기준 분봉</div>
+      <div className="button_container" ref={minuteGroupRef}>
+        <button id="minute_1" onClick={() => onClickMinute(1)}>
+          1분
         </button>
-        <button id="min_3" onClick={() => onHandleClick(3)}>
-          3
+        <button id="minute_3" onClick={() => onClickMinute(3)}>
+          3분
         </button>
-        <button id="min_5" onClick={() => onHandleClick(5)}>
-          5
+        <button id="minute_5" onClick={() => onClickMinute(5)}>
+          5분
         </button>
-        <button id="min_10" onClick={() => onHandleClick(10)}>
-          10
+        <button id="minute_10" onClick={() => onClickMinute(10)}>
+          10분
         </button>
-        <button id="min_15" onClick={() => onHandleClick(15)}>
-          15
+        <button id="minute_15" onClick={() => onClickMinute(15)}>
+          15분
         </button>
-        <button id="min_30" onClick={() => onHandleClick(30)}>
-          30
+        <button id="minute_30" onClick={() => onClickMinute(30)}>
+          30분
+        </button>
+      </div>
+      <br />
+      <div className="button_title">검색할 코인 갯수</div>
+      <div className="button_container" ref={showCountGroupRef}>
+        <button id="showCount_3" onClick={() => onClickShowCount(3)}>
+          3개
+        </button>
+        <button id="showCount_5" onClick={() => onClickShowCount(5)}>
+          5개
+        </button>
+        <button id="showCount_7" onClick={() => onClickShowCount(7)}>
+          7개
+        </button>
+        <button id="showCount_10" onClick={() => onClickShowCount(10)}>
+          10개
+        </button>
+        <button id="showCount_15" onClick={() => onClickShowCount(15)}>
+          15개
+        </button>
+        <button id="showCount_20" onClick={() => onClickShowCount(20)}>
+          20개
         </button>
       </div>
     </div>
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,8 +1,32 @@
+const getStorage = async (key: string) => {
+  const storage = await chrome.storage.local.get([key]);
+  return storage;
+};
+
 setInterval(() => {
-  const distribution = document.getElementsByClassName('ciq-menu ciq-period')[0];
+  const distribution = document.getElementsByClassName("ciq-menu ciq-period")[0];
   if (!distribution) return;
-  const target = distribution.getElementsByTagName('span')[0];
+  const target = distribution.getElementsByTagName("span")[0];
   if (!target) return;
 
-  console.log((target as HTMLElement).innerText);
+  const text = (target as HTMLElement).innerText;
+  (target as HTMLElement).style.color = "red";
+
+  getStorage("minute").then((storage) => {
+    if (storage.minute) {
+      console.log(storage.minute);
+    } else {
+      console.log("storage.minute is undefined");
+    }
+  });
+
+  getStorage("showCount").then((storage) => {
+    if (storage.showCount) {
+      console.log(storage.showCount);
+    } else {
+      console.log("storage.showCount is undefined");
+    }
+  });
+
+  console.log("contentScript.ts is running...");
 }, 1000);
